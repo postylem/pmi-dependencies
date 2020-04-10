c6719ef1e7d95a2c703076b7cb27c8f0073f0f22
Ubuntu <alsordon@alsordon-collab-mila-vm1.bi3tmedyhe3ujij4xoxyqdt5ib.bx.internal.cloudapp.net>
2020-04-10 18:32:20 +0000

    xlm wordpair data gotten

--- run_batch.sh
+++ run_batch.sh
@@ -7,7 +7,7 @@ python pmi-accuracy/main.py --model_spec bert-large-cased --pad 60 --batch_size 
 #python pmi-accuracy/main.py --model_spec xlm-mlm-en-2048 --pad 60 --batch_size 4 > xlm-pad60.out 2> xlm-pad60.err
 #python pmi-accuracy/main.py --model_spec bert-base-cased --pad 30 --batch_size 16 > bert-base-cased-pad30.out 2> bert-base-cased-pad30.err
 #python pmi-accuracy/main.py --model_spec bert-large-cased --pad 30 --batch_size 10 > bert-large-cased-pad30.out 2> bert-large-cased-pad30.err
-#python pmi-accuracy/main.py --model_spec xlnet-base-cased --pad 30 --batch_size 16 > xlnet-base-pad30.out 2> xlnet-base-pad30.err
+python pmi-accuracy/main.py --model_spec xlnet-base-cased --pad 30 --batch_size 16 > xlnet-base-pad30.out 2> xlnet-base-pad30.err
 #python pmi-accuracy/main.py --model_spec xlnet-large-cased --pad 30 --batch_size 10 > xlnet-large-pad30.out 2> xlnet-large-pad30.err
 #python pmi-accuracy/main.py --model_spec xlnet-large-cased --pad 30 --batch_size 10 > xlnet-large-pad30.out 2> xlnet-large-pad30.err
 
