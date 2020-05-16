c43ae67286ef54487c0c27149b4341ab00148c20
Ubuntu <alsordon@alsordon-collab-mila-vm1.bi3tmedyhe3ujij4xoxyqdt5ib.bx.internal.cloudapp.net>
2020-05-16 15:26:32 +0000

    run_batch.sh

--- run_batch.sh
+++ run_batch.sh
@@ -2,12 +2,18 @@
 
 # keep track of things I've run, for pseudologlik by commenting them out for now
 
+#python pmi-accuracy/main.py --model_spec distilbert-base-cased --pad 100 --batch_size 8 > distilbert-pad100.out 2> distilbert-pad100.err
+#python pmi-accuracy/main.py --model_spec distilbert-base-cased --pad 60 --batch_size 24 > distilbert-pad60.out 2> distilbert-pad60.err
 #python pmi-accuracy/main.py --model_spec bert-base-cased --pad 60 --batch_size 16 > bert-base-cased-pad60.out 2> bert-base-cased-pad60.err
-python pmi-accuracy/main.py --model_spec bert-large-cased --pad 60 --batch_size 4 > bert-large-cased-pad60.out 2> bert-large-cased-pad60.err
+#python pmi-accuracy/main.py --model_spec bert-large-cased --pad 60 --batch_size 4 > bert-large-cased-pad60.out 2> bert-large-cased-pad60.err
 #python pmi-accuracy/main.py --model_spec xlm-mlm-en-2048 --pad 60 --batch_size 4 > xlm-pad60.out 2> xlm-pad60.err
 #python pmi-accuracy/main.py --model_spec bert-base-cased --pad 30 --batch_size 16 > bert-base-cased-pad30.out 2> bert-base-cased-pad30.err
 #python pmi-accuracy/main.py --model_spec bert-large-cased --pad 30 --batch_size 10 > bert-large-cased-pad30.out 2> bert-large-cased-pad30.err
-python pmi-accuracy/main.py --model_spec xlnet-base-cased --pad 30 --batch_size 16 > xlnet-base-pad30.out 2> xlnet-base-pad30.err
+#python pmi-accuracy/main.py --model_spec xlnet-base-cased --pad 30 --batch_size 16 > xlnet-base-pad30.out 2> xlnet-base-pad30.err
 #python pmi-accuracy/main.py --model_spec xlnet-large-cased --pad 30 --batch_size 10 > xlnet-large-pad30.out 2> xlnet-large-pad30.err
 #python pmi-accuracy/main.py --model_spec xlnet-large-cased --pad 30 --batch_size 10 > xlnet-large-pad30.out 2> xlnet-large-pad30.err
+#python pmi-accuracy/main.py --model_spec w2v --pad 0 > w2v.out 2> w2v.err
 
+# # Running bert-base-uncased to compare with checkpoints:
+
+#python pmi-accuracy/main.py --model_spec bert-base-uncased --pad 60 --batch_size 16 > bert-base-uncased-pad60.out 2> bert-base-uncased-pad60.err
