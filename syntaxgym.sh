489863254b35f650c9dff1b7a6ef76bba7235d92
Ubuntu <alsordon@alsordon-collab-mila-vm1.bi3tmedyhe3ujij4xoxyqdt5ib.bx.internal.cloudapp.net>
2020-05-17 17:31:48 +0000

    UPDATE sg.sh

--- syntaxgym.sh
+++ syntaxgym.sh
@@ -4,8 +4,8 @@
 # models
 modelArray=("xlnet-base-cased" "bert-large-cased" "xlm-mlm-en-2048" "bart-large" "distilbert-base-cased")
 for model in ${modelArray[*]};do
-  python pmi-accuracy/txt_to_pmi.py --txt SyntaxGym_test_suites/txt/ --model_spec $model
+  python pmi-accuracy/txt_to_pmi.py --txt SyntaxGym_test_suites/txt/ --model_spec "$model" > "SG-${model}.out"
 done
 
-python pmi-accuracy/txt_to_pmi.py --txt SyntaxGym_test_suites/txt/ --model_spec w2v --model_path gensim_w2v.txt
+python pmi-accuracy/txt_to_pmi.py --txt SyntaxGym_test_suites/txt/ --model_spec w2v --model_path gensim_w2v.txt > SG-gensim_w2v.out
 
